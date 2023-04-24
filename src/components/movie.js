78820b6152f0ccda511a48bf48d3ee5c89978ab3
Wesley B. Johnson <wesleyjohnson773@gmail.com>
2023-04-23 18:15:48 -0600

    fghj

--- src/components/movie.js
+++ src/components/movie.js
@@ -9,14 +9,14 @@ import MovieDetail from "../components/moviedetail"
 function Movie(props) {
     const [selectedMovie] = useState(props.selectedMovie);
     const params = useParams();
-    const movieID = params.movieId;
+    const movieId = params.movieId;
     console.log(movieId);
     const dispatch = useDispatch();
     if (selectedMovie == null) {
         dispatch(fetchMovie(movieId));
     }
 
-    return (<MovieDetail movieId={movieID} />)
+    return (<MovieDetail movieId={movieId} />)
 }
 
 export default Movie;
